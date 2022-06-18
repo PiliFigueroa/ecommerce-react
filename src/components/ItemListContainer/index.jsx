2d6bc366bb0c06f2e1dab32ad5d5513f4ed1c19c
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2022-06-18 17:32:13 -0300

    cleaning code

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,29 +1,10 @@
 import { useState } from 'react'
-import { Button } from '@chakra-ui/react'
 
 const ItemListContainer = () => {
-    const valorInicial = "Pilar"
-    //const state = useState(valorInicial)
-    const [nombre, setNombre] = useState(valorInicial)
-
-    const handleClick = () => setNombre("Victoria")
-
-    // const obj = {
-    //     nombre: "Pilar",
-    //     apellido: "Figueroa"
-    // }
-
-    // const { nombre, apellido } = obj
-
-    // console.log(nombre)
-    // console.log(apellido)
-
-    // console.log(state)
 
     return(
         <div>
-            <h1>{nombre}</h1>
-            <Button onClick={handleClick}>Cambiar nombre</Button>
+            Welcome
         </div>
     )
 }
