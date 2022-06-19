fc7e1feb54ad63bc6d12fedb435dac2b89f561f7
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2022-06-19 14:55:50 -0300

    adding context methods

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -6,10 +6,39 @@ export const useCartContext = () => useContext(CartContext)
 export function CartContextProvider({children}) {
 
     const [cartList, setCartList] = useState([])
+    const [cartPrice, setCartPrice] = useState(0)
+
+    const isInCart = (id) => cartList.find(prod => prod.id === id)
+
+    const addToCart = (item, quantity) => {
+      if (isInCart(item.id)) {
+        const newCart = cartList.map(prod => {
+          if (prod.id === item.id) {
+            const newQuantity = prod.quantity + quantity
+            return {item, quantity: newQuantity}
+          } else {
+            return item
+          }
+        })
+        setCartList(newCart)
+      } else {
+        setCartList([...cartList, item])
+      }
+      setCartPrice(cartList.map(prod => cartPrice + (prod.price * prod.quantity)))
+    }
+
+    const cleanCart = () => setCartList([])
+
+    const removeProduct = (id) => setCartList(cartList.filter(prod => prod.id != id))
+
 
     return (
         <CartContext.Provider value={{
-          
+          cartList,
+          addToCart,
+          cleanCart,
+          removeProduct,
+          cartPrice
         }}>
           {children}
         </CartContext.Provider>
